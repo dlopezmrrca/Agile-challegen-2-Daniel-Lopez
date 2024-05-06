8929e5365bc4981054492d70cc796924fbd31581
Daniel <dlopezmaldonado@rrc.ca>
2024-05-06 08:52:09 -0500

    challenge 0 creation

---
+++ Challenges/Challenge-0/ch0.rb
@@ -0,0 +1,14 @@
+# Daniel Lopez
+# Agile Full Stack
+# Challenge 0
+
+# 1
+
+sub_total = 10
+PST = 0.07
+GST = 0.05
+grand_total = (sub_total * PST) + (sub_total * GST) + sub_total
+puts "The Grand Total is, #{grand_total}"
+
+#2
+

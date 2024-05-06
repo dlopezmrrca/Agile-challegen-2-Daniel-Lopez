af9bed3f0cf9c5b518684b3f14520ce1ba2fc735
Daniel <dlopezmaldonado@rrc.ca>
2024-05-06 09:26:14 -0500

    Challenge 0 complete

--- Challenges/Challenge-0/ch0.rb
+++ Challenges/Challenge-0/ch0.rb
@@ -4,11 +4,27 @@
 
 # 1
 
-sub_total = 10
+puts "Enter a Value:"
+user_input = gets.chomp
+sub_total = user_input.to_f
 PST = 0.07
 GST = 0.05
-grand_total = (sub_total * PST) + (sub_total * GST) + sub_total
-puts "The Grand Total is, #{grand_total}"
+
+get_pst = PST * sub_total
+get_gst = GST * sub_total
+grand_total = get_gst + get_pst + sub_total
+
+puts "Subtotal: $#{sub_total} \nPST: $#{get_pst.round(3)} - 7% \nGST: $#{get_gst} - 5% \nGrand Total: $#{grand_total}"
 
 #2
 
+if grand_total <= 5
+  puts "Pocket Change"
+elsif grand_total > 5 && grand_total < 20
+  puts "Wallet Time"
+else
+  puts "Charge It"
+end
+
+#3
+#used chomp

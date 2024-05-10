9b3926ebc2893886e91c8522e405c4d5964ffad0
Daniel <dlopezmaldonado@rrc.ca>
2024-05-10 15:38:34 -0500

    Working on 2 and 3

---
+++ Challenges/Challenge-1/dogs.rb
@@ -0,0 +1,14 @@
+# Daniel Lopez
+# Agile Full Stack
+# Challenge 1
+#3
+
+require 'net/http'
+require 'json'
+require 'pp'
+
+url = 'https://dog.ceo/api/breeds/list/all'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
+pp dog_breeds # pp stands for pretty print.

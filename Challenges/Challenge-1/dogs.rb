70d077eb21d63a0ffea63ef0dd4069b05e85eb76
Daniel <dlopezmaldonado@rrc.ca>
2024-05-14 08:47:32 -0500

    working on dogs file

--- Challenges/Challenge-1/dogs.rb
+++ Challenges/Challenge-1/dogs.rb
@@ -12,3 +12,12 @@ uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
 pp dog_breeds # pp stands for pretty print.
+
+breeds_hash = dog_breeds['message']
+puts "\n*******************\n"
+breeds_hash.each do |breed, sub_breeds|
+  puts "Breed = #{breed.capitalize}"
+  sub_breeds.each do |sub_breed|
+    puts "  Sub Breed = #{sub_breed.capitalize}"
+  end
+end

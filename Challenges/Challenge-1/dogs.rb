74a6e9501a2e5b356fca2b2ca6d52d9b5718e2b1
Daniel <dlopezmaldonado@rrc.ca>
2024-05-14 09:02:11 -0500

    Finishing trees file and updating dogs file

--- Challenges/Challenge-1/dogs.rb
+++ Challenges/Challenge-1/dogs.rb
@@ -11,7 +11,7 @@ url = 'https://dog.ceo/api/breeds/list/all'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
-pp dog_breeds # pp stands for pretty print.
+# pp dog_breeds # pp stands for pretty print.
 
 breeds_hash = dog_breeds['message']
 puts "\n*******************\n"

0d53ba48b0f86eb2cfc3a70a211b819505231b2d
Daniel <dlopezmaldonado@rrc.ca>
2024-05-18 08:50:18 -0500

    Pangram Completed and test updated

--- Challenges/Challenge-2/OO-Ruby-Challenges/04-pangram/pangram.rb
+++ Challenges/Challenge-2/OO-Ruby-Challenges/04-pangram/pangram.rb
@@ -11,3 +11,9 @@ class Pangram
 end
 
 Pangram.is_pangram?('the quick brown fox jumps over the lazy dog')
+Pangram.is_pangram?('a quick movement of the enemy will jeopardize five gunboats')
+Pangram.is_pangram?('the quick brown fish jumps over the lazy dog')
+Pangram.is_pangram?('the_quick_brown_fox_jumps_over_the_lazy_dog')
+Pangram.is_pangram?('the 1 quick brown fox jumps over the 2 lazy dogs')
+Pangram.is_pangram?('"Five quacking Zephyrs jolt my wax bed."')
+Pangram.is_pangram?('Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.')
